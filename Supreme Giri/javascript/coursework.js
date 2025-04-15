5650994d09804eb825cb92f59fa7f8a126cd761c
supremegiri01 <girisupreme3@gmail.com>
2025-04-15 23:41:36 +0545

    Add files via upload

---
+++ Supreme Giri/javascript/coursework.js
@@ -0,0 +1,11 @@
+
+function cart()
+{
+    //For product page.
+    alert("Added to the cart!!!");
+}
+function submitForm() {
+    // In a real-world scenario, you would perform AJAX request or other form submission logic here
+    // For now, let's just show alerts
+    alert("Form submitted!");
+}
